979e04bdfe87e1c70d746b0723920be88c760a34
ParkSunJung <ParkSunJung@192.168.1.102>
2022-07-08 06:55:55 +0900

    005.나머지 합 구하기(백준 10986)

--- doitCoding/src/doitCoding_03/doit_005_10986.java
+++ doitCoding/src/doitCoding_03/doit_005_10986.java
@@ -1,4 +1,3 @@
-
 package doitCoding_03;
 
 import java.util.Scanner;
@@ -6,36 +5,34 @@ import java.util.Scanner;
 public class doit_005_10986 {
 
 	public static void main(String[] args){
-
-
-		Scanner sc = new Scanner(System.in);
+		
+Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		
-		int arr[] = new int[N+1];
-		int sum[] = new int[N+1];
-		int ans[] = new int[N+1]; // 나머지 배열
-		int num[] = new int[N+1]; 
-		int answer = 0;
+		long sum[] = new long[N+1];
+		long arr[] = new long[M];
+		long answer = 0;
+		int num = 0;
+        
+        for(int i=1; i<N+1; i++) {
+			sum[i] = sum[i-1]+ sc.nextInt();
+		}
 		
-		for(int i=1; i<N+1; i++) {			
-			arr[i] = sc.nextInt();
-			sum[i] = arr[i] + sum[i-1];
-			ans[i] = sum[i] % M;
-			
-			if(ans[i] == 0) answer++;	
-			
-			num[ans[i]]++;	
+		for(int i=1; i<N+1; i++) {
+			num =(int)(sum[i] % M);
+			if(num == 0) answer++;
+			arr[num]++ ;
 		}
 		
 		for(int i=0; i<M; i++) {
-			if(num[i]>1) {
-			answer += (num[i]*(num[i]-1))/2;
-
-			}}
+            if(arr[i]>1){
+			    answer += (arr[i]*(arr[i]-1))/2;
+               }
+            }
 		
-		System.out.println(answer);		
+		System.out.println(answer);
 
 	}	
 }
