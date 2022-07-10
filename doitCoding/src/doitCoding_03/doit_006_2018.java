ae3466f34ef581277c4371ae73f1836583949bed
ParkSunJung <ParkSunJung@192.168.1.102>
2022-07-10 13:08:42 +0900

    006.연속된 자연수의 합 구하기(백준 2018) - 복습

--- doitCoding/src/doitCoding_03/doit_006_2018.java
+++ doitCoding/src/doitCoding_03/doit_006_2018.java
@@ -9,33 +9,36 @@ public class doit_006_2018 {
 
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-				
+		
 		int N = Integer.parseInt(st.nextToken());
-		int start = 1;
+		
+		int start = 1; 
 		int end = 1;
 		int count = 1;
 		int sum = 1;
 		
 		while(end != N) {
-			
 			if(sum == N) {
 				count++;
 				end++;
 				sum = sum + end;
-			} 
-			else if(sum > N) {
-				sum = sum - start;
-				start++;
 			}
-			else {
+			else if(sum < N) {
 				end++;
 				sum = sum + end;
-			} 
-			
+			}
+			else {
+				sum = sum-start;
+				start++;
+			}
+
 		}
 		
 		System.out.println(count);
+
+
 	}	
 }
