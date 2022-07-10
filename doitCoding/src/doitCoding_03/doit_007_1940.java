5cf4c8eff13fe570fc71858e30732c266f264ade
PARKSUNJUNG <tjswjd094@gmail.com>
2022-07-10 13:36:45 +0900

    007.주몽의 명령(백준 1940) - 복습

--- doitCoding/src/doitCoding_03/doit_007_1940.java
+++ doitCoding/src/doitCoding_03/doit_007_1940.java
@@ -12,30 +12,29 @@ public class doit_007_1940 {
 		int M = sc.nextInt();
 		int arr[] = new int[N];
 		int start = 0;
+		int end = 0;
 		int ans = 0;
+		
 		
 		for(int i=0; i<N; i++) {
 			arr[i] = sc.nextInt();
 		}
-	
+		
 		for(start=0; start<N-1; start++) {
+			end = start+1;
 			
-			int end = start+1;
-
-			while(end < N){
-				if(arr[start] + arr[end] == M) {
-					ans++;
+			while(end != N) {
+				if(arr[start] + arr[end] ==  M) {
+					ans++;					
 					break;
 				} else {
 					end++;
 				}
-			}
-
+			}			
 		}
-		
 		System.out.println(ans);
+	
 
 	}
-
 
 }
