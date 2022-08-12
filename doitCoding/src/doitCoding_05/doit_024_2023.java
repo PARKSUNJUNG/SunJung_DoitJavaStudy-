a06dfa88c05af10e5fb55add746e622200db2d0e
PARKSUNJUNG <tjswjd094@gmail.com>
2022-08-12 19:58:32 +0900

    24.신기한 소수 찾기(백준 2023) - 복습

--- doitCoding/src/doitCoding_05/doit_024_2023.java
+++ doitCoding/src/doitCoding_05/doit_024_2023.java
@@ -4,47 +4,60 @@ import java.util.Scanner;
 
 public class doit_024_2023 {
 	
-	static int N;
-	
+	public static int N=0;
+
  	public static void main(String[] args)  {
  		
  		Scanner sc = new Scanner(System.in);
  		
  		N = sc.nextInt();
  		
+ 		if(N == 1) {
+ 			System.out.println(2);
+ 			System.out.println(3);
+ 			System.out.println(5);
+ 			System.out.println(7);
+ 			
+ 		}
+ 		
  		DFS(2,1);
  		DFS(3,1);
  		DFS(5,1);
  		DFS(7,1);
+
 	}
  	
- 	static void DFS(int number, int digit) {
+ 	static void DFS(int prime, int count) {
  		
- 		if(digit == N) {
- 			if(isPrime(number)) System.out.println(number);
- 			
- 			return;
- 		}
- 			
- 		for(int i=1; i<10;i++) {
+ 		if(count == N+1) return;
+ 		
+ 		prime = prime*10;
+ 		count++;
+ 		
+ 		for(int i=1; i<=9; i++) {
  			if(i%2 == 0) continue;
  			else {
- 				if(isPrime(number*10 + i)) {
- 					DFS(number*10 + i, digit+1);
- 				}
+	 			int primeNum = prime+i;
+	 			boolean bl = numMethod(primeNum); 			
+	 			if(bl == true) {
+	 				if(count == N) System.out.println(primeNum);
+	 				else {
+	 					DFS(primeNum, count);
+	 				}
+	 			}
  			}
+	
  		}
-		
  	}
  	
- 	static boolean isPrime(int num) {
+ 	static boolean numMethod(int num) {
  		
- 		for(int i=2; i<= num/2; i++) {
- 			if(num%i == 0) 
- 				return false;
+ 		for(int i=2; i<=num/2; i++) {
+ 			if(num%i == 0) return false;
  		}
+ 		
  		return true;
-	
+ 		
  	}
-	
+ 		
 }
