9362546ef642e3aa8a0ace2f4909e1a32b73f8ad
PARKSUNJUNG <tjswjd094@gmail.com>
2023-10-18 12:10:03 +0900

    doit_009_12891_풀이1 - 시간 초과

--- doitCoding/src/doitCoding_03/doit_009_12891.java
+++ doitCoding/src/doitCoding_03/doit_009_12891.java
@@ -5,63 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class doit_009_12891 { 
-
+// 풀이 1 - 시간초과
+public class doit_009_12891 {
+	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int S = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-		int ans = 0;
+		int s = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
 		String str = st.nextToken();
-		
+		char[] ch = str.toCharArray();
+	
+		int[] num = new int[4];
+		int[] num2 = new int[4]; // 비교 배열
 		st = new StringTokenizer(br.readLine());
-		int engNum[] = new int[4];
-		for(int i=0; i<4; i++) { // a,c,g,t 개수
-			engNum[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		char charArr[] = new char[str.length()];
-		for(int i=0; i<str.length(); i++) {
-			charArr[i] = str.charAt(i);
+		for(int i=0; i<4; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		int start = 0;
-		int end = P-1;
-		int chk[] = new int[4];
-
-		while(end != S) {
-			if(start == 0) {
-				for(int i=0; i<P; i++) {		
-					if(charArr[i] == 'A') chk[0]++; 
-					else if(charArr[i] == 'C') chk[1]++; 
-					else if(charArr[i] == 'G') chk[2]++; 
-					else if(charArr[i] == 'T') chk[3]++;
-				}
+		int end = start + (p-1);
+		int ans = 0;
+		
+		while(end < s){
+			for(int i=start; i<=end; i++) {
+				if(ch[i] == 'A') num2[0]++;
+				else if(ch[i] == 'C') num2[1]++;
+				else if(ch[i] == 'G') num2[2]++;
+				else num2[3]++;
 			}
-			else {
-				if(charArr[start-1] == 'A') chk[0]--; 
-				else if(charArr[start-1]== 'C') chk[1]--; 
-				else if(charArr[start-1] == 'G') chk[2]--; 
-				else if(charArr[start-1] == 'T') chk[3]--;
-				
-				if(charArr[end] == 'A') chk[0]++; 
-				else if(charArr[end] == 'C') chk[1]++; 
-				else if(charArr[end] == 'G') chk[2]++; 
-				else if(charArr[end] == 'T') chk[3]++;
-
+			
+			if(num[0] <= num2[0] && num[1] <= num2[1] && num[2] <= num2[2] && num[3] <= num2[3]) {
+				ans++;
+				start++;
+				end++;
+				num2[0]=0; num[1]=0; num2[2]=0; num2[3]=0;
+			} else {
+				start++;
+				end++;
+				num2[0]=0; num[1]=0; num2[2]=0; num2[3]=0;
 			}
-			if(engNum[0] <= chk[0] && engNum[1] <= chk[1] && engNum[2] <= chk[2] && engNum[3] <= chk[3]) ans++;
-			start++;
-			end++;
+			
 		}
 		
 		System.out.println(ans);
-		
 	}
+		
 }
