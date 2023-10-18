6c440cb977d31f98345f05dc276268564facfb18
PARKSUNJUNG <tjswjd094@gmail.com>
2023-10-18 18:15:37 +0900

    doit_009_12891_정답 스스로 풀어보기_90% 풀었음_복습 필요

--- doitCoding/src/doitCoding_03/doit_009_12891.java
+++ doitCoding/src/doitCoding_03/doit_009_12891.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 풀이 2 - 정답 공부
+// 풀이 3 - 정답 스스로 풀어보기_90% 풀었음_복습 필요
 public class doit_009_12891 {
 	
-	static int[] num; // 조건 배열
-	static int[] check; // 체크 배열
-	static int checkNum;
+	static int[] num;
+	static int[] check;
+	static int finish=0; // 최소조건인 4가 충족되면 비밀번호로 사용할 수 있다.
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -19,88 +19,73 @@ public class doit_009_12891 {
 		
 		int s = Integer.parseInt(st.nextToken());
 		int p = Integer.parseInt(st.nextToken());
-		char[] ch = br.readLine().toCharArray();
 		
-		st = new StringTokenizer(br.readLine());
+		char[] ch = br.readLine().toCharArray();
 		num = new int[4];
-		check = new int[4]; // 체크 배열
-		checkNum = 0; // 4가 되었을 때, 조건 충족
+		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<4; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
-			// 문자가 0이면 굳이 체크를 안해도 되므로 미리 걸러내기
-			if(num[i] == 0) checkNum++;
+			if(num[i]==0) finish++;
 		}
 		
-		int ans = 0;
-		
-		// 초기 p 부분 처리
+		check = new int[4]; // 비교 배열
 		for(int i=0; i<p; i++) {
 			Add(ch[i]);
 		}
 		
-		if(checkNum == 4) ans++;
+		int ans=0;
+		if(finish == 4) ans++;
 		
-		for(int i=p; i<s; i++) {
-			int j=i-p;
+		for(int i=p;i<s;i++) {
 			Add(ch[i]);
-			Remove(ch[j]);
-			
-			if(checkNum == 4) ans++;
+			Remove(ch[i-p]);		
+			if(finish == 4) ans++;
 		}
 		
 		System.out.println(ans);
-		br.close();	
-		
+	}	
+	
+	private static void Add(char c) {		
+		switch(c) {
+			case 'A':
+				check[0]++;
+				if(num[0]==check[0]) finish++;
+				break;
+			case 'C':
+				check[1]++;
+				if(num[1]==check[1]) finish++;
+				break;
+			case 'G':
+				check[2]++;
+				if(num[2]==check[2]) finish++;
+				break;
+			case 'T':
+				check[3]++;
+				if(num[3]==check[3]) finish++;
+				break;
+		}
+				
 	}
 	
-	// 새로 들어온 문자를 처리하는 함수 
-	private static void Add(char c) {
+	private static void Remove(char c) {		
 		switch(c) {
-		case 'A':
-			check[0]++;
-			if(num[0] == check[0]) checkNum++;
-			break;
-			
-		case 'C':
-			check[1]++;
-			if(num[1] == check[1]) checkNum++;
-			break;
-			
-		case 'G':
-			check[2]++;
-			if(num[2] == check[2]) checkNum++;
-			break;
-			
-		case 'T':
-			check[3]++;
-			if(num[3] == check[3]) checkNum++;
-			break;
+			case 'A':
+				if(num[0]==check[0]) finish--;
+				check[0]--;
+				break;
+			case 'C':
+				if(num[1]==check[1]) finish--;
+				check[1]--;
+				break;
+			case 'G':
+				if(num[2]==check[2]) finish--;
+				check[2]--;
+				break;
+			case 'T':
+				if(num[3]==check[3]) finish--;
+				check[3]--;
+				break;
 		}
+				
 	}
-	
-	// 기존 문자를 처리하는 함수 
-	private static void Remove(char c) {
-		switch(c) {
-		case 'A':			
-			if(num[0] == check[0]) checkNum--;
-			check[0]--;
-			break;
-			
-		case 'C':
-			if(num[1] == check[1]) checkNum--;
-			check[1]--;
-			break;
-			
-		case 'G':
-			if(num[2] == check[2]) checkNum--;
-			check[2]--;
-			break;
-			
-		case 'T':
-			if(num[3] == check[3]) checkNum--;
-			check[3]--;
-			break;
-		}
-	}
-		
 }
