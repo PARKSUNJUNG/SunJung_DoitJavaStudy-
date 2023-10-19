8084120275f8d75793224f5dc9938535def6eb94
PARKSUNJUNG <tjswjd094@gmail.com>
2023-10-20 04:48:32 +0900

    doit_011_1874_doit_011_1874

--- doitCoding/src/doitCoding_03/doit_011_1874.java
+++ doitCoding/src/doitCoding_03/doit_011_1874.java
@@ -3,48 +3,42 @@ package doitCoding_03;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class doit_011_1874 { 
-
-	public static void main(String[] args)  {
+// ㅊ
+public class doit_011_1874 {
+	
+	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
 		
-		int N = sc.nextInt();
-		
-		int arr[] = new int[N];
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<n; i++) {
 			arr[i] = sc.nextInt();
- 		}
+		}
 		
-		int num= 1;
-		int su = 0;
 		Stack<Integer> stack = new Stack<>();
 		StringBuffer sb = new StringBuffer();
-		Boolean result = true; 
-		
-		for(int i=0; i<N; i++) {
-			su = arr[i];
-			if(su >= num) {
-				while(su >= num) {
-					stack.push(num++);
+		int num=1; // 오름차순의 시작 수
+		for(int i=0; i<n; i++) {
+			if(arr[i] >= num) {
+				while(arr[i] >= num) {
+					stack.push(num++);		
 					sb.append("+\n");
 				}
 				stack.pop();
 				sb.append("-\n");
-			} 
-			else {
-				int n = stack.pop();
-				
-				if(su < n) {
-					result= false;
+			} else {
+				int top = stack.peek();
+				if(top>arr[i]) {
 					System.out.println("NO");
-					
-					break;
+					return;
 				}
+				stack.pop();	
 				sb.append("-\n");
-				
 			}
 		}
-		if(result == true) System.out.println(sb.toString());
+		System.out.println(sb.toString());
+		
 	}
+	
 }
